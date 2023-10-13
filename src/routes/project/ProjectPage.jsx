0ec270d9fbd9e643c84668cd1ad8cac4369e17cd
iamvince24 <yohongguo24@gmail.com>
2023-10-13 22:22:12 +0800

    Modify file structure and Adding Scss methids

--- src/routes/project/ProjectPage.jsx
+++ src/routes/project/ProjectPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Bar from "../../component/bar/Bar.component";
-import ProjectCard from "../../component/projectcard/Projectcard.component";
 import ReReProjectPicture from "../../assets/img/ReRe.Project.Picture.png";
 
 function Project() {

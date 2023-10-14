9f92fd11a19cd70d3b436ef3949236c9102c5af5
iamvince24 <yohongguo24@gmail.com>
2023-10-14 20:59:35 +0800

    Adding RWD design

--- src/component/projectcard/projectcard.component.jsx
+++ src/component/projectcard/projectcard.component.jsx
@@ -9,13 +9,17 @@ function ProjectCard({
   Github,
 }) {
   const tagArray = tagName;
-  const item = tagArray.map((name) => <div className="tagName">{name}</div>);
+  const item = tagArray.map((name, id) => (
+    <div className="tagName" key={id}>
+      {name}
+    </div>
+  ));
 
   return (
     <div className="projectCard" key={projectCardName}>
       <div className="projectCardContent">
         <div className="">
-          <p className="projectCardName">{projectCardName}</p>
+          <div className="projectCardName">{projectCardName}</div>
           <div className="projectCardLinksList">
             <a
               href={Demo}
@@ -23,9 +27,9 @@ function ProjectCard({
               rel="noreferrer"
               className="projectCardLinks"
             >
-              Demo
+              Demo |&nbsp;
             </a>
-            |&nbsp;
+
             <a
               href={Github}
               target="_blank"
@@ -34,12 +38,12 @@ function ProjectCard({
             >
               Github
             </a>
+            <div className="tagContainer">{item}</div>
           </div>
           <p className="projectCardIntroduciton">{introduciton}</p>
         </div>
-        <div className="tagContainer">{item}</div>
       </div>
-      <img src={img} className="projectCardImg" alt="111" />
+      <img src={img} className="projectCardImg" alt="projectCardImg" />
     </div>
   );
 }
--- src/routes/home/HomePage.jsx
+++ src/routes/home/HomePage.jsx
@@ -4,13 +4,18 @@ function Home() {
   return (
     <Fragment>
       <main>
-        <section class="HomePage">
-          <div class="HomePageTitle">
-            Hi ! Everyone,
-            <br />
-            My Name is Vince.
+        <section className="HomePage">
+          <div className="HomePageContent">
+            <div className="HomePageTitle">
+              Hi ! Everyone,
+              <br />
+              My Name is Vince.
+            </div>
+            <div className="HomePageContext">
+              Contact me here vince.guo24@gmail.com
+            </div>
           </div>
-          <div class="HomePageIntroduction">
+          <div className="HomePageIntroduction">
             目前正在尋找前端工程師職位的機會。
             <br />
             作為一名擁有兩年平面設計經驗的設計師，我擅長透過溝通需求，設計出具有美感且注重體驗的視覺。然而，為了做出讓更多人使用的產品，決定轉職軟體產業。而因為學習的熱忱與對該領域的興趣，所以選擇攻讀研究所，以快速地瞭解該領域的基礎學科。
@@ -18,6 +23,9 @@ function Home() {
             Tailwind CSS 來管理元件樣式，以增加開發效率。具有串接平台 API
             的經驗，也會利用有後端服務的平台（比如 Firebase ）來做相關應用。
             目前，我的職業目標是在新的前端工程師職位上，充分發揮我作為設計師的經驗和溝通能力，結合我自學所獲得的前端技術知識，以降低團隊間的溝通成本，達到更有效率的開發，對團隊做出貢獻。
+            <div className="HomePageContextMobile">
+              Contact me here vince.guo24@gmail.com
+            </div>{" "}
           </div>
         </section>
       </main>

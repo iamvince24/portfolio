eadbe7823482ed2d33322c85431ae76b42e55b54
iamvince24 <yohongguo24@gmail.com>
2023-10-26 14:42:20 +0800

    Adding Footer

--- src/routes/home/HomePage.jsx
+++ src/routes/home/HomePage.jsx
@@ -1,45 +1,50 @@
 import React, { Fragment } from "react";
 
+import Footer from "./footer/Footer";
+
 function Home() {
   return (
     <Fragment>
-      <main className="HomePage">
-        <section className="HomePageVision">
-          <div className="HomePageCard1">
-            <div className="HomePageCardTitle">
-              <div className="HomePageCardTitleLine"></div>
-              <div className="HomePageCardTitleText">
-                <div className="HomePageCardTitleHello">Hello! Everyone!</div>
-                <div className="HomePageCardTitleName">
-                  My Name is <span className="HomePageCardTitleNameV">V</span>
-                  ince.
+      <main className="">
+        <section className="HomePage">
+          <section className="HomePageVision">
+            <div className="HomePageCard1">
+              <div className="HomePageCardTitle">
+                <div className="HomePageCardTitleLine"></div>
+                <div className="HomePageCardTitleText">
+                  <div className="HomePageCardTitleHello">Hello! Everyone!</div>
+                  <div className="HomePageCardTitleName">
+                    My Name is <span className="HomePageCardTitleNameV">V</span>
+                    ince.
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="HomePageCardContext">
-              I am looking for a position as a Front-end engineer.
-              <br />
-              Contact me here vince.guo24@gmail.com
-            </div>
-          </div>
-          <div className="HomePageCard2">
-            <div className="HomePageCardTitle">
-              <div className="HomePageCardTitleLine"></div>
-              <div className="HomePageCardTitleText">
-                <div className="HomePageCardTitleHello">Hello! Everyone!</div>
-                <div className="HomePageCardTitleName">
-                  My Name is <span className="HomePageCardTitleNameV">V</span>
-                  ince.
-                </div>
+              <div className="HomePageCardContext">
+                I am looking for a position as a Front-end engineer.
+                <br />
+                Contact me here vince.guo24@gmail.com
               </div>
             </div>
-            <div className="HomePageCardContext">
-              I am looking for a position as a Front-end engineer.
-              <br />
-              Contact me here vince.guo24@gmail.com
+            <div className="HomePageCard2">
+              <div className="HomePageCardTitle">
+                <div className="HomePageCardTitleLine"></div>
+                <div className="HomePageCardTitleText">
+                  <div className="HomePageCardTitleHello">Hello! Everyone!</div>
+                  <div className="HomePageCardTitleName">
+                    My Name is <span className="HomePageCardTitleNameV">V</span>
+                    ince.
+                  </div>
+                </div>
+              </div>
+              <div className="HomePageCardContext">
+                I am looking for a position as a Front-end engineer.
+                <br />
+                Contact me here vince.guo24@gmail.com
+              </div>
             </div>
-          </div>
+          </section>
         </section>
+        <Footer />
       </main>
     </Fragment>
   );
---
+++ src/routes/home/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+function Footer() {
+  return (
+    <Fragment>
+      <section className="FooterSection">
+        <div className="FooterText">
+          Designed in Figma and built with React @ Vince Guo 2023
+        </div>
+      </section>
+    </Fragment>
+  );
+}
+
+export default Footer;

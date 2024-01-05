68204c707a90b28e5070ca6e604e4d72449de390
iamvince24 <yohongguo24@gmail.com>
2024-01-06 01:03:15 +0800

    Update New Project

--- src/routes/project/ProjectPage.jsx
+++ src/routes/project/ProjectPage.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import Bar from "../../component/bar/Bar.component";
 import ProjectCard from "../../component/projectcard/projectcard.component";
 import ReReProjectPicture from "../../assets/img/ReRe.Project.Picture.png";
-import JavaScript20 from "../../assets/img/20_Vanilla_JavaScript_projects.png";
+// import JavaScript20 from "../../assets/img/20_Vanilla_JavaScript_projects.png";
+import FrontEnter from "../../assets/img/front-enter.png";
 
 function Project() {
   return (
@@ -20,14 +21,14 @@ function Project() {
         Github={"https://github.com/iamvince24/Re-Re"}
       />
       <ProjectCard
-        projectCardName={"JavaScript 20"}
+        projectCardName={"Front-Enter"}
         tagName={["JavaScript", "SCSS"]}
         introduction={
-          "These projects are some exercises for JavaScript Web Projects: 20 Projects to Build Your Portfolio."
+          "Front-Enter is a resource-providing website that collects various resources that may be used in the process of learning front-end development. It aims to help users choose the most suitable resources by providing detailed course information and addressing their questions. Additionally, users can share their course experiences and provide feedback."
         }
-        img={JavaScript20}
-        Demo={"https://github.com/iamvince24/JavaScript-20"}
-        Github={"https://github.com/iamvince24/JavaScript-20"}
+        img={FrontEnter}
+        Demo={"https://front-enter.vercel.app/"}
+        Github={"https://github.com/iamvince24/Front-Enter"}
       />
     </div>
   );
